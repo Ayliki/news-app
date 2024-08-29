5798cac6faa98643e424cfb4b1b9c3f117cc39a9
Ilya Belov <ilyabelov518@gmail.com>
2024-08-29 10:22:32 +0200

    added latest news

--- src/components/latestNews/LatestNews.jsx
+++ src/components/latestNews/LatestNews.jsx
@@ -1,10 +1,16 @@
+import { getLatestNews } from '../../api/apiNews'
+import { useFetch } from '../../helpers/hooks/useFetch'
 import BannersList from '../bannersList/BannersList'
 import cl from './styles.module.css'
 
-const LatestNews = ({banners, isLoading}) =>{
+const LatestNews = () =>{
+
+
+    const {data, isLoading} = useFetch(getLatestNews)
+
     return(
         <section className={cl.section}>
-            <BannersList banners={banners} isLoading={isLoading} />
+            <BannersList banners={data && data.news} isLoading={isLoading} />
         </section>
     )
 }
